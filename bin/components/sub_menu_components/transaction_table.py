069f6f57bf1fec33b46ba6899f0aff54d6776abf
shengyu98 <shengyu98@hotmail.com>
2021-11-22 20:57:55 +0800

    upload data and minor fix

--- bin/components/sub_menu_components/transaction_table.py
+++ bin/components/sub_menu_components/transaction_table.py
@@ -1,6 +1,7 @@
 from components.dbconnection import *
 import inquirer as iq
 
+
 def transaction_table():
     print("viewing transaction table")
     year = iq.text(message="Enter a year <2021>")
@@ -10,7 +11,7 @@ def transaction_table():
                 "t.credit_amount, t.description_1, t.description_2, c.category_name " +
                 " FROM transaction_data t, user_detail u, category c "+
                 "WHERE t.account_id = u.account_id AND c.category_id = t.category_id " + 
-                " AND u.email=:email AND CAST(t.transaction_date as character varying(50)) LIKE ':year-:month-%'") 
+                " AND u.email=:email AND CAST(t.transaction_date as character varying(50)) LIKE ':year-%:month-%'") 
     stmt = stmt.columns(transaction.c.transaction_id, transaction.c.transaction_date, transaction.c.debit_amount, transaction.c.credit_amount, transaction.c.description_1, transaction.c.description_2, category.c.category_name)
     stmt = stmt.bindparams(year=int(year), month=int(month), email=email)
     results = session.query(transaction.c.transaction_id, transaction.c.transaction_date, transaction.c.debit_amount, transaction.c.credit_amount, transaction.c.description_1, transaction.c.description_2, category.c.category_name).from_statement(stmt).all()
