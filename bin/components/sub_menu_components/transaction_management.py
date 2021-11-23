05be0b2709e163b3c60b8834744f776062f3f4b8
bryanong120 <75029798+bryanong120@users.noreply.github.com>
2021-11-23 17:39:43 +0800

    updated the delete transaction

--- bin/components/sub_menu_components/transaction_management.py
+++ bin/components/sub_menu_components/transaction_management.py
@@ -146,6 +146,28 @@ def checkCatAndName(u_email,u_id):
 
 def deleteTransaction(u_email):
     uEmail = u_email
+    print("viewing transaction table")
+    year = iq.text(message="Enter a year <2021>")
+    month = iq.text(message="Enter a month <10>")
+
+    print("Transactions for user: ", u_email)
+    
+    stmt7 = text("SELECT t.transaction_id, t.transaction_date, t.debit_amount, t.credit_amount, t.description_1, t.description_2, c.category_name " + \
+    "FROM transaction_data AS t " + \
+    "JOIN category AS c " + \
+    "ON t.category_id = c.category_id " + \
+    "WHERE t.account_id = (SELECT user_detail.account_id FROM user_detail WHERE user_detail.email = :userEmail) " + \
+    "AND CAST(t.transaction_date as character varying(50)) LIKE ':year-%:month-%' " + \
+    "Order By t.transaction_id ASC")
+    stmt7 = stmt7.columns(transaction.c.transaction_id, transaction.c.transaction_date, transaction.c.debit_amount, transaction.c.credit_amount, transaction.c.description_1, transaction.c.description_2, category.c.category_name)
+    stmt7 = stmt7.bindparams(userEmail = u_email, year = int(year), month = int(month))
+
+    results3 = session.query(transaction.c.transaction_id, transaction.c.transaction_date, transaction.c.debit_amount, transaction.c.credit_amount, transaction.c.description_1, transaction.c.description_2, category.c.category_name).from_statement(stmt7).all()
+    print("{:<5}{:<13}{:<12}{:<12}{:<40}{:<40}{:<5}".format("id","date","debit", "credit", "description_1","description_2", "category"))
+    print("_________________________________________________________________________________________________________________________________________")
+    for i in results3:
+        print("{:<5}{:<13}{:<12}{:<12}{:<40}{:<40}{:<5}".format(str(i.transaction_id), str(i.transaction_date), str(i.debit_amount), str(i.credit_amount), str(i.description_1), str(i.description_2), str(i.category_name)))
+
     tID = input("Please enter the transaction ID that you wish to delete: ")
 
     stmt = text("SELECT transaction_data.transaction_id " +
