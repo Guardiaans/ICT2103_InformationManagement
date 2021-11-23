d024e3026d04845ec7e95d8d7862253297bb8949
bryanong120 <75029798+bryanong120@users.noreply.github.com>
2021-11-23 22:48:59 +0800

    updated view_prediction.py, split the prediction data by statements instead

--- RDMS_Postgres/bin/components/main_menu_components/view_prediction.py
+++ RDMS_Postgres/bin/components/main_menu_components/view_prediction.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from sqlalchemy import *
-from components.dbconnection import transaction, session
+from components.dbconnection import transaction, session, category
 import inquirer as iq
 
 def viewPrediction(user_profile):
@@ -27,14 +27,22 @@ def predictData(userEmail):
 
     ''')
    
-    stmt = text("SELECT SUM(transaction_data.credit_amount)/3 AS prediction " + \
+    stmt = text("SELECT category.category_name, SUM(transaction_data.debit_amount)/3 AS prediction " + \
     "FROM transaction_data " + \
+    "JOIN category " + \
+    "ON category.category_id = transaction_data.category_id " + \
     "WHERE transaction_data.transaction_date <= :todayTime " + \
     "AND transaction_data.transaction_date >= :n_days_agoTime " + \
-    "AND transaction_data.account_id = (SELECT user_detail.account_id FROM user_detail WHERE user_detail.email = :mainUser)")
+    "AND transaction_data.account_id = (SELECT user_detail.account_id FROM user_detail WHERE user_detail.email = :mainUser) " + \
+    "GROUP BY category.category_name HAVING SUM(transaction_data.debit_amount)/3 > '0'")
 
-    stmt = stmt.columns(transaction.c.credit_amount)
+    stmt = stmt.columns(category.c.category_name, transaction.c.debit_amount)
     stmt = stmt.bindparams(todayTime = today, n_days_agoTime = n_days_ago, mainUser = userEmail)
-    results = session.query(transaction.c.credit_amount).from_statement(stmt).all()
+    results = session.query(category.c.category_name, transaction.c.debit_amount).from_statement(stmt).all()
+
+    print("Your prediction for this month: ")
+    print("{:<20}{:<12}".format("Category", "Amount"))
+    print("______________________________________________________________________")
     for i in results:
-        print("\nYour current prediction of total spending for this month is: ", (str(i.credit_amount)), "\n\n")
+        print("{:<20}: {:<12}".format(str(i.category_name), str(i.debit_amount)))
+    print("\n")
