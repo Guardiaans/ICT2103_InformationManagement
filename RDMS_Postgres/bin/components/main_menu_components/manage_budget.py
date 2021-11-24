7b9f7ed761776dc6b5fa736e219e906e9f83a7f4
ackillben1 <angyimin98@gmail.com>
2021-11-24 10:52:50 +0800

    Manage budget feature

--- RDMS_Postgres/bin/components/main_menu_components/manage_budget.py
+++ RDMS_Postgres/bin/components/main_menu_components/manage_budget.py
@@ -2,22 +2,17 @@ from components.dbconnection import category, budget, user, session
 import inquirer as iq
 import components.utils as ut
 import sqlalchemy as sq
+from components.sub_menu_components.budget_management import checkCatName, deleteBudget
+
 
 def manageBudget(user_profile):
-    #TODO: 
-    # Creating of sub menu for managing of budget
-    # Implement create budget method
-    # Implementing deleting of budget method
-    
-    # print("managing budget!")
-
     ut.screen_clear()
-    #Menu options for user
+    # Menu options for user
     menuoption = iq.list_input("Managing Budget!",
-                              choices=['Create budget', 
-                              'Delete budget',
-                              'Back',
-                              ])
+                               choices=['Create budget',
+                                        'Delete budget',
+                                        'Back',
+                                        ])
 
     if (menuoption == 'Create budget'):
         budgetAmount = iq.text(message="Enter budget amount")
@@ -29,10 +24,11 @@ def manageBudget(user_profile):
                        ", b.actual_spent, c.category_name FROM budget b, user_detail u, category c "
                        "WHERE b.account_id = u.account_id AND c.category_id = b.category_id "
                        "AND u.email=:email")
-        
+
+        checkCatName(user_profile)
+
     elif (menuoption == 'Delete budget'):
-        #implement delete transaction method.
-        print("delete budget method here")
+        deleteBudget(user_profile)
 
     else:
         pass
