7b87801e4f81057bc36d2e4e2045b5c3fa62a57b
ackillben1 <angyimin98@gmail.com>
2021-11-26 00:26:53 +0800

    Fix budget features

--- NoSQL_Mongo/bin/components/main_menu_components/view_budget.py
+++ NoSQL_Mongo/bin/components/main_menu_components/view_budget.py
@@ -2,12 +2,16 @@ from components.dbconnection import budget
 
 # VIEW BUDGET FUNCTION
 def viewBudget(user_profile):
-    name = user_profile.account_name
-    results = budget.find({ "name": name },
-                          { "_id": 0, "budget_amount": 1, "description": 1, "actual_spent": 1, "month": 1, "year": 1 })
-    print("\n{:<20}{:<20}{:<10}{:<10}{:<100}".format("budget_amount", "actual_spent", "month", "year", "description"))
-    print(
-        "____________________________________________________________________________________________________________________________________________")
-    for i in results:
-        print("${:<19}${:<19}{:<10}{:<10}{:<100}".format(i["budget_amount"], i["actual_spent"], i["month"], i["year"],
-                                                         i["description"]))
+    try:
+        name = user_profile.name
+        results = budget.find({ "name": name },
+                              { "_id": 0, "budget_amount": 1, "description": 1, "actual_spent": 1, "month": 1, "year": 1 })
+        print("\n{:<20}{:<20}{:<10}{:<10}{:<100}".format("budget_amount", "actual_spent", "month", "year", "description"))
+        print(
+            "____________________________________________________________________________________________________________________________________________")
+        for i in results:
+            print("${:<19}${:<19}{:<10}{:<10}{:<100}".format(i["budget_amount"], i["actual_spent"], i["month"], i["year"],
+                                                             i["description"]))
+
+    except:
+        print("Error has occurred!")
