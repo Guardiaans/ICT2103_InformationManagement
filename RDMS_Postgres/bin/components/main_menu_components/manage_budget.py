2f1e999c06c4e6c55b136137258d4f55ecbb2c8c
ackillben1 <angyimin98@gmail.com>
2021-11-25 17:56:18 +0800

    Manage budget feature for NoSQL

--- RDMS_Postgres/bin/components/main_menu_components/manage_budget.py
+++ RDMS_Postgres/bin/components/main_menu_components/manage_budget.py
@@ -15,16 +15,6 @@ def manageBudget(user_profile):
                                         ])
 
     if (menuoption == 'Create budget'):
-        budgetAmount = iq.text(message="Enter budget amount")
-        monthInsert = iq.text(message="Enter month number")
-        yearInsert = iq.text(message="Enter year")
-        descInsert = iq.text(message="Enter description")
-        categoryInsert = iq.text(message="Enter category name")
-        stmt = sq.text("INSERT INTO budget VALUES(?, ?, ?, ?) "
-                       ", b.actual_spent, c.category_name FROM budget b, user_detail u, category c "
-                       "WHERE b.account_id = u.account_id AND c.category_id = b.category_id "
-                       "AND u.email=:email")
-
         checkCatName(user_profile)
 
     elif (menuoption == 'Delete budget'):
